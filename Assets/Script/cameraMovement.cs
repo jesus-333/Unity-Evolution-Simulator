d94c258a0cd5b5b901a22c19c1fdfe3fcebb802d
jesus-333 <albi25.zanca@gmail.com>
2021-09-26 02:17:41 +0200

    Initial commit asset

---
+++ Assets/Script/cameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for mouse input wheels
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f ) {
+
+        } else if (Input.GetAxis("Mouse ScrollWheel") < 0f ) {
+
+        }
+
+        if(Input.GetMouseButton(1)){
+            print("RIGHT");
+        }
+    }
+}
