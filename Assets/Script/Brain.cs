b1864d59576a641e35d0f49ebeea44f4d9125233
jesus-333 <albi25.zanca@gmail.com>
2021-11-06 12:06:12 +0100

    Work on movement
    - Create basic code that allow the creature to move based on the value of output neurons
    - Correct minor bug in neurons vector creation
    - Divide Brain code in section

--- Assets/Script/Brain.cs
+++ Assets/Script/Brain.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Brain : MonoBehaviour
 {
     public int n_genes = 8;
+    public float speed = 1f;
 
     public InputNeuron[] input_neurons;
     public HiddenNeuron[] hidden_neurons, output_neurons;
 
+    private Vector3 move_vec;
+    private CharacterController controller;
+
     public bool debug_var;
 
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Unity related methods
 
     void Start()
     {
@@ -18,19 +25,30 @@ public class Brain : MonoBehaviour
         InitInputNeurons();
 
         // Inizialization of output neurons
-        output_neurons = InitHiddenNeurons(2, 0f);
+        output_neurons = InitHiddenNeurons(2, 0.5f);
         /*
         Output Neurons list
         0 ---> up/down
         1 ---> left/right
         */
+
+        // Retrive CharacterController
+        controller = this.GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        // Update input neurons state
         input_neurons[0].updateState();
+
+        // Move the creature
+        Move();
+
         // Debug();
     }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Init methods
 
     /*
     Initializes the input neurons one by one. This is necessary due to the fact that input neurons can have particular inizialization.
@@ -44,27 +62,49 @@ public class Brain : MonoBehaviour
     }
 
     /*
-    Method used to generate an array of HiddenNeuron. Used for both hidden neurons and output neurons since the their update logic is the same.
+    Method used to generate an array of HiddenNeuron with a given state. Used for both hidden neurons and output neurons since the their update logic is the same.
     */
     public HiddenNeuron[] InitHiddenNeurons(int n_neurons, float state){
         HiddenNeuron[] tmp_neurons_array = new HiddenNeuron[n_neurons];
 
         for (int i = 0; i < n_neurons; i++){
             if(state > 1f || state < - 1f){ // Init with random state
-                tmp_neurons_array[0] = new HiddenNeuron();
+                tmp_neurons_array[i] = new HiddenNeuron();
             } else { // Init with a specific state
-                tmp_neurons_array[0] = new HiddenNeuron(state);
+                tmp_neurons_array[i] = new HiddenNeuron(state);
             }
         }
 
         return tmp_neurons_array;
     }
 
+    /*
+    Method used to generate an array of HiddenNeuron with random state. Used for both hidden neurons and output neurons since the their update logic is the same.
+    */
+    public HiddenNeuron[] InitHiddenNeurons(int n_neurons){
+        return InitHiddenNeurons(n_neurons, -2);
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Action methods
+
+    public void Move(){
+        int tmp_threeshold = 20;
+        if(this.transform.position.x > tmp_threeshold || this.transform.position.x < - tmp_threeshold){ output_neurons[0].state *= - 1; }
+        if(this.transform.position.z > tmp_threeshold || this.transform.position.z < - tmp_threeshold){ output_neurons[1].state *= - 1; }
+
+        move_vec = new Vector3(output_neurons[0].state, 0, output_neurons[1].state);
+        controller.Move(move_vec * Time.deltaTime * speed);
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Other methods
 
     /*
     Method used to print information and perform debugging
     */
     public void Debug(){
-        // print("Objective Neuron state:\t" + input_neurons[0].state);
+        for(int i = 0; i < 2; i++){print(i + " " + output_neurons[i].state);}
     }
 }
--- Assets/Script/cameraMovement.cs
+++ Assets/Script/cameraMovement.cs
@@ -6,7 +6,7 @@ public class cameraMovement : MonoBehaviour
 {
 
     public float scroll_speed = 10f;
-    public Camera camera;
+    public Camera camera_var;
 
     private Vector3 start_point, end_point;
 
@@ -20,22 +20,22 @@ public class cameraMovement : MonoBehaviour
 
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         // Zoom
-        if(camera.orthographic){
-            camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scroll_speed;
+        if(camera_var.orthographic){
+            camera_var.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scroll_speed;
         } else {
-            if(camera.fieldOfView >= 1) { camera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * scroll_speed; }
-            else { camera.fieldOfView = 1f; }
+            if(camera_var.fieldOfView >= 1) { camera_var.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * scroll_speed; }
+            else { camera_var.fieldOfView = 1f; }
         }
 
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        Vector3 camera_translation = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f) * camera.fieldOfView / 60;
-        camera.transform.Translate(camera_translation);
+        Vector3 camera_translation = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f) * camera_var.fieldOfView / 60;
+        camera_var.transform.Translate(camera_translation);
 
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         // Draw safe zone
         if(Input.GetMouseButtonDown(0)){
             print(Input.mousePosition);
-            print(camera.ViewportToWorldPoint(Input.mousePosition));
+            print(camera_var.ViewportToWorldPoint(Input.mousePosition));
         }
 
         if(Input.GetMouseButton(0)){
