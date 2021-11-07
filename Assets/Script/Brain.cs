914dacd2351cbe7a50bb71158cf69173600913b5
jesus-333 <albi25.zanca@gmail.com>
2021-11-07 11:11:05 +0100

    Add support file, genes init
    - Add a script support file to write functions that don't fit inside normal class but that can be useful
    - Write and test genes init. The number of genes are divided between links and hidden neurons (different methods possible) and a string is created to describe the results. Future methods could be added in future

--- Assets/Script/Brain.cs
+++ Assets/Script/Brain.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Brain : MonoBehaviour
 {
-    public int n_genes = 8;
+    public int n_genes = 8, n_links = 0, n_hidden_neurons = 0, genes_mode_init = 2;
     public float speed = 4f;
+
+    public string genes;
 
     public InputNeuron[] input_neurons;
     public HiddenNeuron[] hidden_neurons, output_neurons;
@@ -21,6 +23,9 @@ public class Brain : MonoBehaviour
 
     void Start()
     {
+        // Inizialization of the genes
+        InitGenes(genes_mode_init);
+
         // Inizialization of input neurons
         InitInputNeurons();
 
@@ -51,6 +56,40 @@ public class Brain : MonoBehaviour
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // Init methods
+
+    /*
+    Divide the number of genes between links and hidden neurons with the constraint that n_links + n_hidden_neurons = n_genes.
+    */
+    public void InitGenes(int mode){
+        switch(mode){
+            // Even split (more or less) between hidden neurons and links.
+            case 0:
+                n_links = n_genes / 2;
+                n_hidden_neurons = n_genes - n_links; // To account odd number of genes.
+            break;
+
+            // Randomly split between hidden neurons and links. 0 hidden_neurons or 0 links are possible.
+            case 1:
+                n_links = Random.Range(0, n_genes + 1);
+                n_hidden_neurons = n_genes - n_links;
+            break;
+
+            // Randomly split between hidden neurons and links. 0 hidden_neurons or 0 links are NOT possible.
+            case 2:
+                n_links = Random.Range(1, n_genes);
+                n_hidden_neurons = n_genes - n_links;
+            break;
+
+            // Defualt case if the wrong mode is used.
+            default:
+                if(debug_var){ print("Wrong number for mode selection. Use case 0 as defualt"); }
+                n_links = n_genes / 2;
+                n_hidden_neurons = n_genes - n_links; // To account odd number of genes.
+            break;
+        }
+
+        genes = supportMethods.IntToCharLowerCase(n_links) + supportMethods.IntToCharLowerCase(n_hidden_neurons);
+    }
 
     /*
     Initializes the input neurons one by one. This is necessary due to the fact that input neurons can have particular inizialization.
@@ -91,14 +130,22 @@ public class Brain : MonoBehaviour
         return InitHiddenNeurons(n_neurons, -2);
     }
 
+    /*
+    Method used to create the connections between the neurons.
+    */
+    public void InitWiring(){
+
+    }
+
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // Action methods
 
     public void Move(){
-        int tmp_threeshold = 20;
-        if(this.transform.position.x > tmp_threeshold || this.transform.position.x < - tmp_threeshold){ output_neurons[0].state *= - 1; }
-        if(this.transform.position.z > tmp_threeshold || this.transform.position.z < - tmp_threeshold){ output_neurons[1].state *= - 1; }
+        // Temporary code to test movement
+        // int tmp_threeshold = 20;
+        // if(this.transform.position.x > tmp_threeshold || this.transform.position.x < - tmp_threeshold){ output_neurons[0].state *= - 1; }
+        // if(this.transform.position.z > tmp_threeshold || this.transform.position.z < - tmp_threeshold){ output_neurons[1].state *= - 1; }
 
         // Evaluate direction based on output neurons and move in that direction
         move_vec = new Vector3(output_neurons[0].state, 0, output_neurons[1].state);
@@ -113,7 +160,7 @@ public class Brain : MonoBehaviour
     // Other methods
 
     /*
-    Method used to print information and perform debugging
+    Method used to print information and perform debugging.
     */
     public void Debug(){
         // Print of movement neurons state
---
+++ Assets/Script/Support.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// Files used to define extra functions/class
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*
+(NOT WORKING FOR NOW)
+// Class to define an attribute read only in the inspector. Find on answer unity forum
+https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+*/
+// public class ReadOnlyAttribute : PropertyAttribute
+// {
+//
+// }
+//
+// [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+// public class ReadOnlyDrawer : PropertyDrawer
+// {
+//     public override float GetPropertyHeight(SerializedProperty property,
+//                                             GUIContent label)
+//     {
+//         return EditorGUI.GetPropertyHeight(property, label, true);
+//     }
+//
+//     public override void OnGUI(Rect position,
+//                                SerializedProperty property,
+//                                GUIContent label)
+//     {
+//         GUI.enabled = false;
+//         EditorGUI.PropertyField(position, property, label, true);
+//         GUI.enabled = true;
+//     }
+// }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+public static class supportMethods{
+
+    /*
+    Convert an int between 0 and 25 into a lower case string
+    */
+    public static string IntToCharLowerCase(int x){
+        if(x < 0 || x > 25){ throw new ArgumentException("Invalid value for the input x", nameof(x)); }
+
+        char tmp_c;
+        tmp_c = (char)(x + 97);
+        return tmp_c + "";
+    }
+
+    /*
+    Convert an int between 0 and 25 into a upper case string
+    */
+    public static string IntToCharUpperCase(int x){
+        if(x < 0 || x > 25){ throw new ArgumentException("Invalid value for the input x", nameof(x)); }
+
+        char tmp_c;
+        tmp_c = (char)(x + 65);
+        return tmp_c + "";
+    }
+
+    /*
+    Convert an int between 0 and 51 into a upper case string
+    0 to 25 will be converted in lower case while 26 to 51 in upper case
+    */
+    public static string IntToChar(int x){
+        if(x < 0 || x > 51){ throw new ArgumentException("Invalid value for the input x", nameof(x)); }
+
+        char tmp_c;
+        if(x >= 25){ tmp_c = (char)(x + 97); }
+        else{ tmp_c = (char)(x - 26 + 65); }
+
+        return tmp_c + "";
+    }
+}
